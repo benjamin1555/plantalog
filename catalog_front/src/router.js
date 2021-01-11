750b5745d5b5b6ac93ece761b7c3b21c1018d45e
Benjamin Teste <benjamin.teste@gmail.com>
2021-01-11 11:35:43 -0500

    add navigation guards on restricted routes

--- catalog_front/src/router.js
+++ catalog_front/src/router.js
@@ -8,26 +8,43 @@ import EditPlant from './pages/EditPlant.vue';
 import Login from './pages/Login.vue';
 import SignUp from './pages/SignUp.vue';
 import NotFound from './pages/NotFound.vue';
+import store from './store/index.js';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/planter'},
-    { path: '/planter', component: Plant},
+    {
+      path: '/',
+      redirect() {
+        return store.getters.isAuthenticated ? '/planter' : '/catalogue';
+      }
+    },
+    { path: '/planter', meta: { needsAuth: true }, component: Plant},
     {
       path: '/catalogue',
       component: Catalog,
       props: true,
       children: [
         { path: 'plants/:id', component: PlantDetail },
-        { path: 'plants/:id/editer', component: EditPlant },
-        { path: 'ajouter-plante', component: AddPlant },
+        { path: 'plants/:id/editer', meta: { needsAuth: true }, component: EditPlant },
+        { path: 'ajouter-plante', meta: { needsAuth: true }, component: AddPlant },
       ]
     },
-    { path: '/login', component: Login },
-    { path: '/signup', component: SignUp },
+    { path: '/login', meta: { needsUnAuth: true }, component: Login },
+    { path: '/signup', meta: { needsUnAuth: true }, component: SignUp },
     { path: '/:notFound(.*)', component: NotFound }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.needsAuth && !store.getters.isAuthenticated) {
+    next('/login');
+  } else if (to.meta.needsUnAuth && store.getters.isAuthenticated) {
+    console.log(to);
+    next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
