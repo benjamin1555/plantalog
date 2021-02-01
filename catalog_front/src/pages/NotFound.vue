9dec9ba9c09719596b8a7951085ecd4b9124197c
Benjamin Teste <benjamin.teste@gmail.com>
2021-02-01 15:28:26 -0500

    change styling of 404 page

--- catalog_front/src/pages/NotFound.vue
+++ catalog_front/src/pages/NotFound.vue
@@ -1,3 +1,25 @@
 <template>
-  <h2>Not Found.</h2>
+  <div>
+    <h1>Erreur 404 🥴</h1>
+    <h3>Cette page n'existe pas !</h3>
+  </div>
 </template>
+
+<style scoped>
+div {
+  margin: auto auto;
+}
+
+h1, h3 {
+  text-align: center;
+}
+
+h1 {
+  font-weight: 300;
+  font-size: 4rem;
+}
+
+h3 {
+  font-weight: 300;
+}
+</style>
